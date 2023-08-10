15ae03fa516f3e46b87fed21a8d7869d250718c6
Anna Krysenko <krysenkoanna@gmail.com>
2023-08-10 19:26:07 +0300

    first commit

---
+++ src/js/accordion.js
@@ -0,0 +1,8 @@
+import Accordion from 'accordion-js';
+new Accordion('.accordion-container');
+import 'accordion-js/dist/accordion.min.css';
+
+// new Accordion(['.first-accordion', '.second-accordion'], {
+//     openOnInit: [0],
+//     collapse: false
+// });
